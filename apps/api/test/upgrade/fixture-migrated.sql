9918058ec3300a09b07e622215ff354d2c1efbac
MrAdex77 <mradex77@gmail.com>
2026-09-15 19:57:59 +0200

    test(db): give the backup drill fixture what the summary migrations change

--- apps/api/test/upgrade/fixture-migrated.sql
+++ apps/api/test/upgrade/fixture-migrated.sql
@@ -5,4 +5,12 @@ SET "fieldOrder" = 0
 WHERE "appId" = 'app_ios'
   AND "keywordId" = 'kw_ios_us';
 
+UPDATE "AppSnapshot"
+SET "summary" = 'Workouts & meals ''daily'', amp tools, R&D &lt;3, ''fit'''
+WHERE "id" = 'snap_play_escaped';
+
+UPDATE "TrackedKeyword"
+SET "active" = false
+WHERE ("appId", "keywordId") IN (('app_play', 'kw_play_amp_meals'), ('app_play', 'kw_play_39_daily'));
+
 COMMIT;
