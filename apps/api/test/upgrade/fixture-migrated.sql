94a1bf9e28adf645de53b70291632d49364c463e
MrAdex77 <mradex77@gmail.com>
2026-09-15 19:56:52 +0200

    test(db): give the backup drill fixture what the identity migrations change

--- apps/api/test/upgrade/fixture-migrated.sql
+++ apps/api/test/upgrade/fixture-migrated.sql
@@ -5,4 +5,12 @@ SET "fieldOrder" = 0
 WHERE "appId" = 'app_ios'
   AND "keywordId" = 'kw_ios_us';
 
+UPDATE "App"
+SET "storeAppId" = '333333333'
+WHERE "id" = 'app_padded';
+
+UPDATE "TrackedKeyword"
+SET "active" = false
+WHERE ("appId", "keywordId") IN (('app_ios', 'kw_ios_zz'), ('app_play', 'kw_play_pw'));
+
 COMMIT;
