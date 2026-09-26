15d5b98ad26d941036d05565acec1f6952f3175f
MrAdex77 <mradex77@gmail.com>
2026-09-26 16:37:14 +0200

    Revert "refactor(web): extract a media query hook from the mobile check"
    This reverts commit 7b0aa0f241089e96dc624a641a430eb2589f693f.

--- apps/web/src/lib/use-is-mobile.ts
+++ apps/web/src/lib/use-is-mobile.ts
@@ -1,8 +1,18 @@
-import { useMediaQuery } from "./use-media-query";
+import { useSyncExternalStore } from "react";
 
 const MOBILE_BREAKPOINT = 768;
 const QUERY = `(max-width: ${MOBILE_BREAKPOINT - 1}px)`;
 
+function subscribe(onStoreChange: () => void) {
+  const query = window.matchMedia(QUERY);
+  query.addEventListener("change", onStoreChange);
+  return () => query.removeEventListener("change", onStoreChange);
+}
+
 export function useIsMobile(): boolean {
-  return useMediaQuery(QUERY);
+  return useSyncExternalStore(
+    subscribe,
+    () => window.matchMedia(QUERY).matches,
+    () => false,
+  );
 }
